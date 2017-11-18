88683d882c5cf677b43c9ab19890741aa2952ad8
Muhammad Atta khan <m.atta.khan@hotmail.com>
2017-11-18 16:52:58 +0500

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,10 @@
+print("hello world")
+
+
+def countdonuts(count):
+    if count < 10:
+        result = "Number of Count:" + str(count)
+    else:
+        result = "Number of Count:" + "many"
+    return result
+
